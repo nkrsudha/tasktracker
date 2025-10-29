ff6fd06c93fcb486edc6e697b894cd3cdf384a62
nkrsudha <nkr.sudha@gmail.com>
2025-10-29 12:04:57 +0100

    Update User entity for better alignment

--- src/main/java/com/sudha/tasktracker/model/User.java
+++ src/main/java/com/sudha/tasktracker/model/User.java
@@ -24,18 +24,48 @@ public class User {
     private List<Task> tasks;
 
     // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() 
+    {
+         return id; 
+    }
+    public void setId(Long id)
+    {
+         this.id = id; 
+    }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getUsername() 
+    { 
+        return username; 
+    }
+    public void setUsername(String username) 
+    { 
+        this.username = username; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() 
+    { 
+       return email; 
+    }
+    public void setEmail(String email) 
+    { 
+        this.email = email; 
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() 
+    { 
+        return password;
+    }
+    public void setPassword(String password) 
+    { 
+        this.password = password; 
+    }
 
-    public List<Task> getTasks() { return tasks; }
-    public void setTasks(List<Task> tasks) { this.tasks = tasks; }
+    public List<Task> getTasks() 
+    { 
+        return tasks;
+    }
+    public void setTasks(List<Task> tasks) 
+    { 
+        this.tasks = tasks; 
+    }
 }
