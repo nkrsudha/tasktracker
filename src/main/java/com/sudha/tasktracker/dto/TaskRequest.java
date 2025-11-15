addea230109e9e9700e1474ce1b793b722d7bd70
nkrsudha <nkr.sudha@gmail.com>
2025-11-15 18:53:34 +0100

    Added TaskRequest DTO for assigning tasks to users

---
+++ src/main/java/com/sudha/tasktracker/dto/TaskRequest.java
@@ -0,0 +1,32 @@
+package com.sudha.tasktracker.dto;
+
+public class TaskRequest {
+
+    private String title;
+    private String description;
+    private Long userId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
